edb48d7b81d3dfb969671cfc1d2b06414955b2d4
EC2 Default User <ec2-user@ip-172-31-64-194.ec2.internal>
2021-04-15 14:52:43 +0000

    Last commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import CurrentOrder from './components/CurrentOrder';
 import OrderForm from "./components/OrderForm";
 
-class App extends React.Component {
-   state = {
-     orders: [],
-     menu: {
-       ramens: ['しょうゆラーメン', 'みそラーメン', 'とんこつラーメン', '富山ブラック', '塩ラーメン', 'つけめん'],
-       sizes: ['小盛', '普通', '大盛り'],
-       chashu: ['1枚', '2枚', '3枚', '山盛り'],
-     }
-  }
-  addOrder = async(order)  => {
-    const orders = [order, ...this.state.orders]
-    this.setState({ orders })
-  }
-  
-  deleteOrder = async(id) => {
-    const orders = this.state.orders.filter(n => n.id !== id)
-    this.setState({ orders })
+function App(props) {
+ const [orders, setOrders] = useState([]);
+ const [menu] = useState({
+     ramens: ['しょうゆラーメン', 'みそラーメン', 'とんこつラーメン', '富山ブラック', '塩ラーメン', 'つけめん'],
+     sizes: ['小盛', '普通', '大盛り'],
+     chashu: ['1枚', '2枚', '3枚', '山盛り'],
+   });
+   
+  const state = {
+    orders: orders,
+    menu: menu
   }
   
-  render() {
-    return (
-      <div className="container">
-        <div class="py-5 text-center">
-          <img class="d-block mx-auto mb-4" src="https://chojugiga.com/c/choju47_0033/s128_choju47_0033_6.png" alt="" width="72" height="72" />
-          <h2>ラーメンBach Khoa</h2>
-      　</div>
-        <div class="row">
-          <div class="col-sm-6">
-            <OrderForm 
-              data={this.state}
-              addOrder={this.addOrder}
-            />
-          </div>
-          <div class="col-sm-6">
-            <CurrentOrder 
-              data={this.state.orders}
-              deleteOrder={this.deleteOrder}
-            />
-          </div>
+  function addOrder(order){
+    setOrders([...orders, order]);
+  }
+      
+  
+  function deleteOrder(id){
+    const filteredOrders = orders.filter(n => n.id !== id);
+    setOrders(filteredOrders);
+  }
+
+  return (
+    <div className="container">
+      <div class="py-5 text-center">
+        <img class="d-block mx-auto mb-4" src="https://chojugiga.com/c/choju47_0033/s128_choju47_0033_6.png" alt="" width="72" height="72" />
+        <h2>ラーメンBach Khoa</h2>
+    　</div>
+      <div class="row">
+        <div class="col-sm-6">
+          <OrderForm 
+            data={state}
+            addOrder={addOrder}
+          />
+        </div>
+        <div class="col-sm-6">
+          <CurrentOrder 
+            data={state.orders}
+            deleteOrder={deleteOrder}
+          />
         </div>
       </div>
-    ) 
-  }
+    </div>
+  ) 
 }
+
 export default App;
--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,74 +1,77 @@
-import React, {Component} from 'react';
+import React, {useRef} from 'react';
 
-class OrderForm extends Component {
-  onSubmit = (e) => {
+function OrderForm(props) {
+  const onSubmit = (e) => {
        e.preventDefault()
       
-      let newOrder = {
-        id: this.props.data.orders.length + 1,
-        ramen: this.refs.ramen.value,
-        size: this.refs.size.value,
-        chashu: this.refs.chashu.value
+      const refs= {
+        ramen:"選んでください",
+        size:"選んでください",
+        chashu:"選んでください"
       }
-      this.props.addOrder(newOrder)
       
-      this.refs.ramen.value="選んでください"
-      this.refs.size.value="選んでください"
-      this.refs.chashu.value="選んでください"
+      const newOrder = {
+        id: props.data.orders.length + 1,
+        ramen: refs.ramen,
+        size: refs.size,
+        chashu: refs.chashu
+      }
+      props.addOrder(newOrder)
+        
   }
 
-  render() {
-    let ramens = this.props.data.menu.ramens
-    let sizes = this.props.data.menu.sizes
-    let chashu = this.props.data.menu.chashu
-    
-    let ramenTypes = ramens.map((ramen, key) => {
-      return <option key={key} value={ramen}>{ramen}</option>
-    })
-    
-    let ramenSizes = sizes.map((size, key) => {
-      return <option key={key} value={size}>{size}</option>
-    })
-    
-    let chashuSpoons = chashu.map((spoon, key) => {
-      return <option key={key} value={spoon}>{spoon}</option>
-    })
-    
-    return (
-      <form
-        onSubmit= {this.onSubmit}>
-        <h4 class="mb-3">注文をどうぞ</h4>
-        <div class="mb-3">
-          <label>どのラーメンにしますか？</label>
-          <select
-            className="custom-select" 
-            ref="ramen" >
-            <option defaultvalue="">選んでください</option>
-            {ramenTypes}
-          </select>
-        </div>            
-        <div class="mb-3">
-          <label>大きさはどうしますか？</label>
-          <select
-            className="custom-select" 
-            ref="size">
-            <option defaultvalue="">選んでください</option>
-            {ramenSizes}
-          </select>
-        </div>            
-        <div class="mb-3">
-          <label>チャーシューは何枚ですか？</label>
-          <select
-            className="custom-select" 
-            ref="chashu">
-            <option defaultvalue="">選んでください</option>
-            {chashuSpoons}
-          </select>
-        </div>            
-        <input className="btn btn-success" type="submit" value="注文" />
-      </form>
-    )
-  }
+
+  const ramens = props.data.menu.ramens
+  const sizes = props.data.menu.sizes
+  const chashu = props.data.menu.chashu
+  
+  const ramenTypes = ramens.map((ramen, key) => {
+    return <option key={key} value={ramen}>{ramen}</option>
+  })
+  
+  const ramenSizes = sizes.map((size, key) => {
+    return <option key={key} value={size}>{size}</option>
+  })
+  
+  const chashuSpoons = chashu.map((spoon, key) => {
+    return <option key={key} value={spoon}>{spoon}</option>
+  })
+  
+  return (
+    <form
+      onSubmit= {onSubmit}>
+      <h4 class="mb-3">注文をどうぞ</h4>
+      <div class="mb-3">
+        <label>どのラーメンにしますか？</label>
+        <select
+          className="custom-select" 
+          ref={useRef("ramen")} >
+          <option defaultvalue="">選んでください</option>
+          {ramenTypes}
+        </select>
+      </div>            
+      <div class="mb-3">
+        <label>大きさはどうしますか？</label>
+        <select
+          className="custom-select" 
+          ref={useRef("size")} >
+          <option defaultvalue="">選んでください</option>
+          {ramenSizes}
+        </select>
+      </div>            
+      <div class="mb-3">
+        <label>チャーシューは何枚ですか？</label>
+        <select
+          className="custom-select" 
+          ref={useRef("chashu")} >
+          <option defaultvalue="">選んでください</option>
+          {chashuSpoons}
+        </select>
+      </div>            
+      <input className="btn btn-success" type="submit" value="注文" />
+    </form>
+  )
 }
+
 
 export default OrderForm;
